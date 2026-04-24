c51a6591ec21ce99fce68a936b43a22529b1c2ce
Dohyung Ahn <dksehgud1@naver.com>
2026-04-24 12:23:32 +0900

    S14P31B109-113 [BE] Project directory 생성

---
+++ BE/biometric-service/src/test/java/com/ssafy/rebloom/biometric_service/BiometricServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.rebloom.biometric_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiometricServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ BE/notification-service/src/main/java/com/ssafy/rebloom/notification_service/NotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.rebloom.notification_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationServiceApplication.class, args);
+	}
+
+}
