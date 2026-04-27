ac83e97cc74dec764047e66935ed30e60f35b436
cramfull <tmdgud99@gmail.com>
2026-04-28 01:09:52 +0900

    S14P31B109-120 [BE] 공통 DTO

---
+++ BE/modules/common-module/src/main/java/com/rebloom/common/dto/PageResponseDto.java
@@ -0,0 +1,20 @@
+package com.rebloom.common.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import java.util.List;
+import org.springframework.data.domain.Page;
+
+public record PageResponseDto<T>(
+    PageInfo page,
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    SortInfo sort,
+    List<?> contents
+) {
+    public static <T> PageResponseDto<T> from(Page<T> page) {
+        return new PageResponseDto<T>(
+            PageInfo.of(page),
+            SortInfo.of(page),
+            page.getContent()
+        );
+    }
+}
