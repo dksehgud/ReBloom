73a10ffeb670bf41a0ad651ae4344adcc46fcaa7
cramfull <tmdgud99@gmail.com>
2026-05-13 13:18:03 +0900

    hotfix: S14P31B109-616 [INFRA] hotfix 로컬배포 오류 수정

--- BE/modules/common-module/src/main/java/com/ssafy/rebloom/common/config/QueryDslConfig.java
+++ BE/modules/common-module/src/main/java/com/ssafy/rebloom/common/config/QueryDslConfig.java
@@ -3,10 +3,12 @@ package com.ssafy.rebloom.common.config;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConditionalOnClass({JPAQueryFactory.class, EntityManager.class})
 public class QueryDslConfig {
 
     @PersistenceContext
